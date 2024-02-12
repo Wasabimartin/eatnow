fd15deefce536be50507afee1e0c6ca182106364
mterens <martin.terens@gmail.com>
2024-02-12 17:22:08 +0100

    Enum Backend

---
+++ src/main/java/com/eatnow/eatnow/dtos/DrinkDTO.java
@@ -0,0 +1,47 @@
+package com.eatnow.eatnow.dtos;
+
+public class DrinkDTO {
+        private String name;
+        private boolean alcohol;
+        private Double size;
+        private String unit;
+
+    public DrinkDTO(String name, boolean alcohol, Double size, String unit) {
+        this.name = name;
+        this.alcohol = alcohol;
+        this.size = size;
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAlcohol() {
+        return alcohol;
+    }
+
+    public void setAlcohol(boolean alcohol) {
+        this.alcohol = alcohol;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
