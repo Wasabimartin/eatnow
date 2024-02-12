cea0a2a6a52b5d35b7d62b7398937ceec948d415
mterens <martin.terens@gmail.com>
2024-02-12 09:43:15 +0100

    Ports

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,39 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [drinks, setDrinks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('api/drinks')
+      .then(response => response.json())
+      .then(data => {
+        setDrinks(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-intro">
+          <h2>Drink List</h2>
+          {drinks.map(drink =>
+            <div key={drink.id}>
+              {drink.id} - {drink.name}
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
